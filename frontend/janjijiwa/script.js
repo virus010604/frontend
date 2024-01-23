a396874db5d1f19d8a5e26223d248315a21ceb1e
virus01064 <jp1jn04@gmail.com>
2024-01-23 18:45:11 +0800

    ubah

--- frontend/janjijiwa/script.js
+++ frontend/janjijiwa/script.js
@@ -3,9 +3,20 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+// tombol search
+const searchform = document.querySelector(".searchform");
+document.querySelector("#searcha").onclick = () => {
+  searchform.classList.toggle("active");
+};
+
+const searchbox = document.querySelector("searchbox");
+document.addEventListener("click", function (e) {
+  if (!searcha.contains(e.target) && !searchform.contains(e.target)) {
+    searchform.classList.remove("active");
+  }
+});
 
 //klik di luar sidebar
-
 const hamburger = document.querySelector("#hamburger");
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
