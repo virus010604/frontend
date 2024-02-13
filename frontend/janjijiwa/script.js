fcfea713f09212d97b6fae47ea4df6e2bbdfce7f
virus01064 <jp1jn04@gmail.com>
2024-02-13 22:13:41 +0800

    add more fitur

--- frontend/janjijiwa/script.js
+++ frontend/janjijiwa/script.js
@@ -5,11 +5,12 @@ document.querySelector("#hamburger").onclick = () => {
 };
 // tombol search
 const searchform = document.querySelector(".searchform");
-document.querySelector("#searcha").onclick = () => {
+const searchbox = document.querySelector("#searchbox");
+document.querySelector("#searcha").onclick = (e) => {
   searchform.classList.toggle("active");
+  searchbox.focus();
+  e.preventDefault();
 };
-
-const searchbox = document.querySelector("searchbox");
 document.addEventListener("click", function (e) {
   if (!searcha.contains(e.target) && !searchform.contains(e.target)) {
     searchform.classList.remove("active");
@@ -21,5 +22,17 @@ const hamburger = document.querySelector("#hamburger");
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+// shop
+const shop = document.querySelector(".shopcart")
+document.querySelector("#shopping-cart").onclick = (e) => {
+  shop.classList.toggle("active");
+  e.preventDefault();
+};
+const cart = document.querySelector("#shopping-cart")
+document.addEventListener("click", function (e) {
+  if (!cart.contains(e.target) && !shop.contains(e.target)) {
+    shop.classList.remove("active");
   }
 });
